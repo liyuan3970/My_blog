9df15507a97f08ef00ed7c667b93834ff61aa992
liyuan3970 <857956255@qq.com>
2019-06-04 23:54:29 +0800

    model

--- My_blog/My_blog/settings/product.py
+++ My_blog/My_blog/settings/product.py
@@ -78,10 +78,18 @@ WSGI_APPLICATION = 'My_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myblog',
+        'USER': 'root',
+        'PASSWORD':123456,
+        'HOST': 'localhost',
+        'PORT':3306,
     }
 }
+
+
+
+
 
 
 # Password validation
