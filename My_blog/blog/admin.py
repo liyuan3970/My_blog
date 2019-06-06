22977594d1ba7057c9a9425969ec6a5103578dfc
liyuan3970 <857956255@qq.com>
2019-06-06 22:15:55 +0800

    readme

--- My_blog/blog/admin.py
+++ My_blog/blog/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import *
+
+
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Tag)
-admin.site.register(Post)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'is_nav', 'created_time')
+    fields = ('name', 'status', 'is_nav')
